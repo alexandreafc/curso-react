50c90da5cfee588bdb5984b633bc7a9752867255
Alexandre Costa <alexandreafc@gmail.com>
2020-11-13 16:21:29 +0000

    Adicionado componente aleatório.

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from './components/basicos/Aleatorio';
+import Primeiro from './components/basicos/Primeiro';
+import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
 
 export default function App(props) {
     return (
         <div id="app">
+            <h1>Fundamentos React</h1>
+            <Aleatorio min={10} max={60} />
             <Fragmento />
             <ComParametro
                 titulo="Situação do Aluno"
