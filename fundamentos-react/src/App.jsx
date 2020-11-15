f685660921f1c652d3fb7e64f97c62738e6fa662
Alexandre Costa <alexandreafc@gmail.com>
2020-11-15 01:23:53 +0000

    Aula 21

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import "./App.css";
+import React from "react";
 
-import Aleatorio from './components/basicos/Aleatorio';
-import Primeiro from './components/basicos/Primeiro';
-import ComParametro from './components/basicos/ComParametro';
-import Fragmento from './components/basicos/Fragmento';
+import Card from "./components/layout/Card";
+import Aleatorio from "./components/basicos/Aleatorio";
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
 
-export default function App(props) {
-    return (
-        <div id="app">
-            <h1>Fundamentos React</h1>
-            <Aleatorio min={10} max={60} />
-            <Fragmento />
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Alexandre" nota={9.5} />
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Pedro" nota={6.5} />
-            <Primeiro></Primeiro>
-        </div>
-    )
-}
+export default () => (
+	<div className='App'>
+		<h1>Fundamentos React</h1>
+
+		<div className='Cards'>
+			<Card titulo='#04 - Desafio Aleatório' color='#FA6900'>
+				<Aleatorio min={10} max={60} />
+			</Card>
+
+			<Card titulo='#03 - Fragmento' color='#E94C6F'>
+				<Fragmento />
+			</Card>
+
+			<Card titulo='#02 - Com Parâmetro' color='#E8B71A'>
+				<ComParametro titulo='Situação do Aluno' aluno='Alexandre' nota={9.5} />
+				<ComParametro titulo='Situação do Aluno' aluno='Pedro' nota={6.5} />
+			</Card>
+
+			<Card titulo='#01 - Primeiro Componente' color='#588C73'>
+				<Primeiro></Primeiro>
+			</Card>
+		</div>
+	</div>
+);
