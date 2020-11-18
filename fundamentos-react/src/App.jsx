127b838acca59793c68f872ba7316129492a758e
Alexandre Costa <alexandreafc@gmail.com>
2020-11-18 00:21:23 +0000

    Até aula 29

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 import React from "react";
 
+import UsuarioInfo from "./components/condicional/UsuarioInfo";
+import ParOuImpar from "./components/condicional/ParOuImpar";
 import Card from "./components/layout/Card";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
+import TabelaProdutos from "./components/repeticao/TabelaProdutos";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
@@ -12,19 +18,36 @@ export default () => (
 		<h1>Fundamentos React</h1>
 
 		<div className='Cards'>
+			<Card titulo='#08 - Renderização Condicional' color='#982395'>
+				<ParOuImpar numero={20}></ParOuImpar>
+				<UsuarioInfo usuario={{ nome: "Alexandre" }} />
+			</Card>
+			<Card titulo='#07 - Desafio Repetição' color='#3A9AD9'>
+				<TabelaProdutos></TabelaProdutos>
+			</Card>
+
+			<Card titulo='#06 - Repetição' color='#FF4C65'>
+				<ListaAlunos></ListaAlunos>
+			</Card>
+
+			<Card titulo='#05 - Componente com Filhos' color='#00C8F8'>
+				<Familia sobrenome='Ferreira'>
+					<FamiliaMembro nome='Pedro' />
+					<FamiliaMembro nome='Ana' />
+					<FamiliaMembro nome='Gustavo' />
+				</Familia>
+			</Card>
+
 			<Card titulo='#04 - Desafio Aleatório' color='#FA6900'>
 				<Aleatorio min={10} max={60} />
 			</Card>
-
 			<Card titulo='#03 - Fragmento' color='#E94C6F'>
 				<Fragmento />
 			</Card>
-
 			<Card titulo='#02 - Com Parâmetro' color='#E8B71A'>
 				<ComParametro titulo='Situação do Aluno' aluno='Alexandre' nota={9.5} />
 				<ComParametro titulo='Situação do Aluno' aluno='Pedro' nota={6.5} />
 			</Card>
-
 			<Card titulo='#01 - Primeiro Componente' color='#588C73'>
 				<Primeiro></Primeiro>
 			</Card>
---
+++ fundamentos-react/src/data/produtos.js
@@ -0,0 +1,13 @@
+const produtos = [
+	{ id: 1, nome: "Caneta", preco: 1.54 },
+	{ id: 1, nome: "Lapis", preco: 0.54 },
+	{ id: 1, nome: "iPad", preco: 1200.0 },
+	{ id: 1, nome: "Samsung S20 Ultra", preco: 2000 },
+	{ id: 1, nome: "Notebook", preco: 3500.54 },
+	{ id: 1, nome: "Borracha", preco: 2.05 },
+	{ id: 1, nome: "Impressora", preco: 400.54 },
+	{ id: 1, nome: "Monitor 27", preco: 500.0 },
+	{ id: 1, nome: "Cadeira", preco: 200.99 },
+];
+
+export default produtos;
