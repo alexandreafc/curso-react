b100978fe32f918e1cda9020d5b0981ab766df48
Alexandre Costa <alexandreafc@gmail.com>
2020-11-19 14:09:21 +0000

    Acerto Id.

--- fundamentos-react/src/data/produtos.js
+++ fundamentos-react/src/data/produtos.js
@@ -1,13 +1,13 @@
 const produtos = [
 	{ id: 1, nome: "Caneta", preco: 1.54 },
-	{ id: 1, nome: "Lapis", preco: 0.54 },
-	{ id: 1, nome: "iPad", preco: 1200.0 },
-	{ id: 1, nome: "Samsung S20 Ultra", preco: 2000 },
-	{ id: 1, nome: "Notebook", preco: 3500.54 },
-	{ id: 1, nome: "Borracha", preco: 2.05 },
-	{ id: 1, nome: "Impressora", preco: 400.54 },
-	{ id: 1, nome: "Monitor 27", preco: 500.0 },
-	{ id: 1, nome: "Cadeira", preco: 200.99 },
+	{ id: 2, nome: "Lapis", preco: 0.54 },
+	{ id: 3, nome: "iPad", preco: 1200.0 },
+	{ id: 4, nome: "Samsung S20 Ultra", preco: 2000 },
+	{ id: 5, nome: "Notebook", preco: 3500.54 },
+	{ id: 6, nome: "Borracha", preco: 2.05 },
+	{ id: 7, nome: "Impressora", preco: 400.54 },
+	{ id: 8, nome: "Monitor 27", preco: 500.0 },
+	{ id: 9, nome: "Cadeira", preco: 200.99 },
 ];
 
 export default produtos;
