eb182e08e91f3ce7dc5007c1ccb26d4b21880bec
Alexandre Costa <alexandreafc@gmail.com>
2020-11-20 23:37:39 +0000

    Adicionado componentes: Button, Display

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,2 +1,20 @@
-import React, { Component } from "react";
-import "./Calculator.css";
+import React from "react";
+import "./Button.css";
+
+const button = (props) => {
+	let classes = "button ";
+	classes += 'button props.operation ? "operation" : ""';
+	classes += 'button props.double ? "double" : ""';
+	classes += 'button props.triple ? "triple" : ""';
+
+	return (
+		<button
+			onClick={(e) => props.click && props.click(props.label)}
+			className={classes}
+		>
+			{props.label}
+		</button>
+	);
+};
+
+export default button;
---
+++ calculadora/src/components/Display.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Display.css";
+
+const display = (props) => <button className='display'>{props.value}</button>;
+
+export default display;
